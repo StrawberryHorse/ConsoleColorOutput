235fa1e1da5adec29bcaec317730a816329380d8
The Strawberry Horse <strawberryhorse@seventhhorseman.net>
2019-10-14 14:07:21 +0300

    Project skeleton

---
+++ src/ConsoleColorOutput/ConsoleColorOutput/ConsoleOutputHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleColorOutput
+{
+    public class ConsoleOutputHelper
+    {
+        public void WriteLine ( string text )
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
--- src/ConsoleColorOutput/TestCCO/Program.cs
+++ src/ConsoleColorOutput/TestCCO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ConsoleColorOutput;
 
 namespace TestCCO
 {
@@ -6,7 +7,8 @@ namespace TestCCO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ou = new ConsoleOutputHelper();
+            ou.WriteLine("{yellow}Hello World!{}");
         }
     }
 }
