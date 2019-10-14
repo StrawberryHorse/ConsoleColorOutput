b001e0542b75bef2c00fc3b40d9bc9d152aaa85c
The Strawberry Horse <strawberryhorse@seventhhorseman.net>
2019-10-14 14:20:58 +0300

    Added some simple test cases on what we want to achieve

--- src/ConsoleColorOutput/TestCCO/Program.cs
+++ src/ConsoleColorOutput/TestCCO/Program.cs
@@ -8,7 +8,20 @@ namespace TestCCO
         static void Main(string[] args)
         {
             var ou = new ConsoleOutputHelper();
+
+            // First test case: we need to change to color with the {color} syntax
             ou.WriteLine("{yellow}Hello World!{}");
+
+            // Second test case: headings
+            ou.WriteLine("# This should be white as it is something important.");
+
+            // Third test case: wrapping 
+            ou.WriteLine(@"This is written in multiple
+lines in the source code. But a single line break should
+not break the output into multiple lines. Instead, the output
+should flow around the console. So this multi line
+message should fit inside the console, and get wrapped
+accordingly.");
         }
     }
 }
